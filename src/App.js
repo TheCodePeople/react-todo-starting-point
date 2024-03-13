3a7995d2fe18ca54adc440b0bd7e8f8eacbbd73b
ZehraaMA <zahraa@thecodepeople.io>
2024-03-13 14:56:05 +0300

    create the starting point of todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// TODO: Import the todoData and pass it as a prop to the TodoList component
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Call the TodoList Component Here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todoData.js
@@ -0,0 +1,34 @@
+const todoData = [
+  {
+    id: 1,
+    title: "Go bungee jumping",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Go bungee jumping",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "Learn to paint a picture",
+    done: false,
+  },
+  {
+    id: 4,
+    title: "Write a book",
+    done: false,
+  },
+  {
+    id: 5,
+    title: "Travel to every continent",
+    done: false,
+  },
+  {
+    id: 6,
+    title: "Skydive from a plane",
+    done: false,
+  },
+];
+
+export default todoData;
