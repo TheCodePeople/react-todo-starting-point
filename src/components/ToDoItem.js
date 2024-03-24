aed200cd04c0cda03be88df44b3328b2706d0c57
mehdi22256 <mehdi22256@gmail.com>
2024-03-24 13:28:22 +0300

    done

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 export const ToDoItem = ({ todo, handleDelete, handleEdit }) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [editedTitle, setEditedTitle] = useState("");
+  const [editedTitle, setEditedTitle] = useState(todo.title);
 
   const handleChange = (event) => {
     setEditedTitle(event.target.value);
@@ -14,7 +14,8 @@ export const ToDoItem = ({ todo, handleDelete, handleEdit }) => {
   return (
     <div className="flex items-center gap-5 py-2">
       <button
-        className="text-white bg-red-900 px-2 rounded-lg"
+        style={{ backgroundColor: "#E10032" }}
+        className="text-white  px-2 rounded-lg"
         onClick={() => handleDelete(todo.id)}
       >
         Delete
@@ -22,13 +23,19 @@ export const ToDoItem = ({ todo, handleDelete, handleEdit }) => {
       <button onClick={handleIsEditing}>
         {isEditing ? (
           <button
-            className="text-white bg-red-900 px-2 rounded-lg"
+            style={{ backgroundColor: "#DBE8E1", color: "#151B25" }}
+            className="text-white  px-2 rounded-lg"
             onClick={() => handleEdit(todo.id, editedTitle)}
           >
             save
           </button>
         ) : (
-          <button className="bg-gray-500 px-2 rounded-lg">Edit</button>
+          <button
+            style={{ backgroundColor: "#DBE8E1", color: "#151B25" }}
+            className=" px-2 rounded-lg"
+          >
+            Edit
+          </button>
         )}
       </button>
       {isEditing ? (
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,28 +1,47 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { ToDoItem } from "./ToDoItem";
 
 function TodoList({ todoData }) {
   const [todoList, setTodoList] = useState(todoData);
   const [newTodo, setNewTodo] = useState("");
-
+  const [error, setError] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  // Create
   const handleTextChange = (event) => {
     setNewTodo(event.target.value);
   };
+  const clear = useRef();
   const handleTodoCreate = () => {
-    const newTodoObj = {
-      id: uuidv4(),
-      title: newTodo,
-      done: false,
-    };
-    // Update the array to add the new item
-    setTodoList([...todoList, newTodoObj]);
+    let errors = "";
+    if (!newTodo.trim()) {
+      errors = "write something";
+    } else if (/\d+/.test(newTodo)) {
+      errors = "must not contain numbers";
+    } else if (/\W+/.test(newTodo)) {
+      errors = "must not contain symbols";
+    } else {
+      const newTodoObj = {
+        id: uuidv4(),
+        title: newTodo,
+        done: false,
+      };
+      // Update the array to add the new item
+      setTodoList([...todoList, newTodoObj]);
+    }
+    setError(errors);
+    if (errors.length === 0) {
+      clear.current.value = "";
+    }
   };
+
+  // Delete
   const handleDelete = (todoId) => {
     setTodoList(todoList.filter((todo) => todo.id !== todoId));
   };
 
+  // Edit
   const handleEdit = (todoId, editedTitle) => {
     setTodoList(
       todoList.map((todo) => {
@@ -31,30 +50,68 @@ function TodoList({ todoData }) {
     );
   };
 
+  // Search
+  useEffect(() => {
+    setFiltered(todoList);
+  }, [todoList]);
+  const handleSearch = (e) => {
+    const filter = todoList.filter((todo) =>
+      todo.title.toLowerCase().includes(e.target.value)
+    );
+    setFiltered(filter);
+  };
+
   return (
     <div>
-      <div className="pb-5 flex justify-center gap-14">
+      <div id="search" className="flex flex-col pb-7 items-center px-16">
         <input
+          style={{ backgroundColor: "#DBE8E1" }}
           type="text"
-          className="text-black rounded-lg px-2"
-          placeholder="Enter your Data"
-          onChange={handleTextChange}
+          className="text-black rounded-lg px-2 py-1"
+          placeholder="Search"
+          onInput={handleSearch}
         />
-        <button
-          className="bg-slate-400 p-2 px-3 rounded-lg"
-          onClick={handleTodoCreate}
-        >
-          Add
-        </button>
       </div>
-      <div className="bg-gray-600 p-5 rounded-lg flex justify-center flex-col">
-        {todoList.map((todo) => (
+      <div
+        style={{ backgroundColor: "#034687" }}
+        className=" p-5 rounded-lg flex justify-center flex-col"
+      >
+        {filtered.map((todo) => (
           <ToDoItem
             todo={todo}
             handleDelete={handleDelete}
             handleEdit={handleEdit}
           />
         ))}
+      </div>
+      <div id="Add" className="pt-7 flex justify-center gap-14">
+        <div className="flex flex-col ">
+          <input
+            ref={clear}
+            type="text"
+            style={{ backgroundColor: "#DBE8E1" }}
+            className="text-black rounded-lg px-2 py-1"
+            placeholder="Enter your Data"
+            onChange={handleTextChange}
+          />
+          {error && (
+            <span
+              style={{ color: "#E10032" }}
+              className=" text-sm text-left pl-2"
+            >
+              {error}
+            </span>
+          )}
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "#034687" }}
+            className=" px-2 py-1 rounded-lg"
+            onClick={handleTodoCreate}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </div>
   );
