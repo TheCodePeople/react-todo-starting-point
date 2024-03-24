360cfde87f0a62ec137b5ca48d6408b608673b4e
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-03-24 18:03:05 +0300

    done todo

--- src/assets/componets/Todo.js
+++ src/assets/componets/Todo.js
@@ -2,35 +2,70 @@ import { useState } from "react";
 
 import uuid4 from "uuid4";
 import TodoItem from "./Todoitem";
+import ssimg from "./img/search (1).png"
+
 function TodoList({ tododata }) {
   const [todoList, setTodoList] = useState(tododata);
-  const [newtodo, setnewtodo] = useState("");
+  const [newTodo, setNewTodo] = useState("");
+  const [searchitem, setsearchitem] = useState("");
 
-  function handelinput(event) {
-    setnewtodo(event.target.value);
-  }
-  function craeatlist() {
-    const newTodo = {
+  const handleInput = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const createList = () => {
+    const newTodoItem = {
       id: uuid4(),
-      title: newtodo,
+      title: newTodo,
       done: false,
     };
-    setTodoList([...todoList, newTodo]);
-  }
-  
-  function deletitems(todoId) {
+    setTodoList([...todoList, newTodoItem]);
+    setNewTodo("");
+  };
+
+  const deleteItem = (todoId) => {
     setTodoList(todoList.filter((todo) => todo.id !== todoId));
-  }
+  };
 
-
-  
   return (
-    <div>
-      <input type="text" onChange={handelinput} />
-      <button onClick={craeatlist}>add</button>
-      {todoList.map((todo) => (
-        <TodoItem todo={todo} deletitems={deletitems} />
-      ))}
+    <div className=" w-{90%} h-full p-4 my-5 rounded-md overflow-y-auto">
+      <div className="w-50 h-40 flex flex-col mb-20">
+        <div className="flex  w-full m-2">
+           <input
+          type="text"
+          value={newTodo}
+          onChange={handleInput}
+          placeholder="Add new task"
+          className="w-full border border-gray-300 rounded text-black "
+        />
+        <button onClick={createList} className=" px-4 bg-green-400 text-white text-2xl rounded ">
+          +
+        </button>
+        
+        </div>
+         <div className="flex  w-full m-2 bg-teal-600 h-1"></div>
+
+         <div className="flex  w-full m-2">
+           <input
+          type="text"
+          id="searchInput"
+          value={searchitem}
+          onChange={(event) => setsearchitem(event.target.value)}
+          placeholder="Search tasks"
+          className="w-full border border-gray-300 rounded text-black "
+        />
+                <label  className="mt-2"><img className="w-11" src={ssimg} alt="" /></label>
+
+         </div>
+       
+      </div>
+
+      {todoList.map((todo) => {
+        if (todo.title.toLowerCase().includes(searchitem.toLowerCase())) {
+          return <TodoItem key={todo.id} todo={todo} deleteItem={deleteItem} />;
+        }
+        return null;
+      })}
     </div>
   );
 }
--- src/assets/componets/Todoitem.js
+++ src/assets/componets/Todoitem.js
@@ -1,37 +1,74 @@
 import React, { useState } from "react";
+import eidt from "./img/edit.png"
+import delet from "./img/trash.png"
 
-function TodoItem({ todo, deletitems }) {
+function TodoItem({ todo, deleteItem }) {
   const [isEditing, setIsEditing] = useState(false);
   const [newTitle, setNewTitle] = useState(todo.title);
-
-  const handleInputChange = (event) => {
-    setNewTitle(event.target.value);
+  const [isChecked, setisChecked] = useState(false);
+  const [greencolor ,setgreencolor] = useState(false)
+  const [yalowcolor ,setyalowcolor] = useState(false)
+  const [redcolor ,setredcolor] = useState(false)
+  const handleSave = () => {
+    todo.title = newTitle;
+    setIsEditing(false);
   };
-
- 
-
+  
   const handleEditClick = () => {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   };
+ 
+  function handelcheckbox (){
+    setisChecked(!isChecked)
+  }
+ 
+  
+ function greenboxcolor (){
+  setgreencolor(!greencolor)
+ }
+ function yaloweboxcolor(){
+  setyalowcolor(!yalowcolor)
+ }
+ function redboxcolor (){
+  setredcolor(!redcolor)
+ }
 
   return (
-    <div>
+    <div >
       {isEditing ? (
-        <div>
+        <div className="flex gap-5 justify-between  bg-white mb-2 p-2 ">
           <input
+          className=" text-black"
             type="text"
             value={newTitle}
-            onChange={handleInputChange}
+            onChange={(event) => setNewTitle(event.target.value)}
           />
-          <sub >Save</sub>
+          <button className="text-green-600" onClick={handleSave}>Save</button>
         </div>
       ) : (
-        <div>
-          <p key={todo.id}>{todo.title}</p>
-          <sub onClick={() => deletitems(todo.id)} style={{ color: "red" }}>
-            delete
-          </sub>
-          <sub onClick={handleEditClick}>Edit</sub>
+            <div className={`flex gap-5 justify-between h-auto w-{100%}  mb-6 p-2 rounded-lg hover:shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] 
+             shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] ${isChecked?"bg-slate-400  text-white" : "bg-white" }`}>
+            
+          <div className="flex gap-3 w-{70%}" >
+             <input type="checkbox" checked={isChecked} onChange={handelcheckbox}/> 
+            <p className={`w-full text-black font-serif text-wrap ${isChecked ? "line-through text-gray-500  " : ""}`}>{todo.title}</p>
+            </div>
+
+            <div className={"flex gap-5 w-{30%}"}>
+            
+          <div className={"flex gap-2 h-5 mt-2"}>
+            <div className={`${greencolor?"bg-green-500 px-2 py-2":"bg-green-100 px-2 py-2"}`} onClick={greenboxcolor}></div>
+            <div  className={`${yalowcolor?"bg-yellow-400 px-2 py-2":"bg-yellow-100 px-2 py-2"}`} onClick={yaloweboxcolor}></div>
+            <div  className={`${redcolor?"bg-red-500 px-2 py-2":"bg-red-100 px-2 py-2"}`} onClick={redboxcolor}></div>
+          </div>
+
+          <div className="flex gap-4">
+            <label htmlFor=""></label>
+            <button className="text-red-600" onClick={() => deleteItem(todo.id)}><img className="w-9" src={delet} alt="" /></button>
+          <button className="text-green-600" onClick={handleEditClick}><img className="w-9" src={eidt} alt="" /></button>
+          </div>
+            </div>
+         
         </div>
       )}
     </div>
